d2ce1a16ee1b9d385d3b25e222fe669265e1ba17
CallMeTrinity <anteur.ap@gmail.com>
2024-05-12 08:35:33 +0200

    questions and first config

---
+++ src/components/Question.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{ question.question }}</p>
+    <select v-model="question.score">
+      <option value="0">0 - Pas du tout moi / Jamais</option>
+      <option value="1">1 - Ça peut m'arriver / Parfois</option>
+      <option value="2">2 - Ça me ressemble / Souvent</option>
+      <option value="3">3 - Tout à fait moi / Toujours</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    question: Object,
+    required: true
+  }
+}
+</script>
---
+++ src/views/TestView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Test de Personnalité</h1>
+    <form @submit.prevent="submitAnswers">
+      <Question v-for="(question, index) in questions" :key="question.id" :question="question" />
+      <button type="submit">Soumettre les réponses</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import Question from '../components/Question.vue';
+import questions from '../data/question.js';
+
+export default {
+  components: {
+    Question
+  },
+  data() {
+    return {
+      questions
+    };
+  },
+  methods: {
+    submitAnswers() {
+      console.log("Réponses soumises:", this.questions);
+      // Ajouter ici la logique pour traiter ou envoyer les réponses
+    }
+  }
+}
+</script>
